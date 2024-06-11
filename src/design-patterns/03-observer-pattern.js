31eb0d9e80dddd96e8383e9b60cf9d1eb4e18d13
zphtet <zinphtet.215108@gmail.com>
2024-06-11 07:46:43 +0630

    add code

--- src/design-patterns/03-observer-pattern.js
+++ src/design-patterns/03-observer-pattern.js
@@ -102,3 +102,36 @@ function Observer(){
         //  console.log("context")
      }
 }
+
+
+
+let controlCheckbox = document.getElementById( "mainCheckbox" ),
+ addBtn = document.getElementById( "addNewObserver" ),
+ container = document.getElementById( "observersContainer" );
+
+ // Extend the controlling checkbox with the Subject class
+extend( new Subject(), controlCheckbox );
+
+
+// Clicking the checkbox will trigger notifications to its observers
+controlCheckbox["onclick"] = new Function( "controlCheckbox.Notify(controlCheckbox.checked)" );
+
+function AddNewObserver(){
+  // Create a new checkbox to be added
+  var check = document.createElement( "input" );
+  check.type = "checkbox";
+  // Extend the checkbox with the Observer class
+  extend( new Observer(), check );
+  // Override with custom update behaviour
+  check.Update = function( value ){
+  this.checked = value;
+  };
+  // Add the new observer to our list of observers
+  // for our main subject
+  controlCheckbox.AddObserver( check );
+  // Append the item to the container
+  container.appendChild( check );
+ }
+
+addBtn["onclick"] = AddNewObserver;
+// Concrete Observer
