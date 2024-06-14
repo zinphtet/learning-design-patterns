ae1f77a8396b059613ea2291808acafb0c17a248
zinphtet <zinpainghtet.215108@gmail.com>
2024-06-14 13:42:51 +0630

    facade pattern

---
+++ src/design-patterns/07-facade-pattern.js
@@ -0,0 +1,18 @@
+
+// THE FACADE PATTERN 
+
+// abstraction kind of   
+// this pattern is we normally used in daily life to hide the detail implementation and export the api 
+
+
+
+
+const addMyEvent = function (el , ev , fn){
+     if(el.addMyEvent){
+         el.addMyEvent(ev, fn , false);
+     }else if (el.attachEvent){
+         el.attachEvent("on" + ev, fn);
+     }else {
+        el["on" + ev] = fn;
+     }
+}
