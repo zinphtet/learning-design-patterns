6e80c1430178f46099be9a2a72d32e4dfd5667e9
zinphtet <zinpainghtet.215108@gmail.com>
2024-06-25 16:08:55 +0630

    flywieght pattern

--- src/design-patterns/09-flyweight-pattern.js
+++ src/design-patterns/09-flyweight-pattern.js
@@ -2,8 +2,47 @@
 // It aims to minimize the use of memory in an application by sharings as much data as possible
 // with related objects. (eg, ap config , state , and so on , ...)
 
-
-// In Flyweight pattern , there is a concept of two states : intric and extric . 
-// Intrinsic info my be required by internal methods in our objects, which they absolutely cannot function without. 
+// In Flyweight pattern , there is a concept of two states : intric and extric .
+// Intrinsic info my be required by internal methods in our objects, which they absolutely cannot function without.
 // Extric info can however be removed and stored externally.
 // Objects with the same intrinsic data can replacced with a single shared objecy , created b a factory method.
+
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+const isbnNumbers = new Set();
+const bookList = [];
+
+const addBook = (title, author, isbn, instock, sales) => {
+  const book = {
+    ...createBook(title, author, isbn),
+    sales,
+    instock,
+    isbn,
+  };
+  bookList.push(book);
+  return book;
+};
+
+const createBook = (title, author, isbn) => {
+  const book = isbnNumbers.has(isbn);
+  if (book) {
+    return book;
+  } else {
+    const book = new Book(title, author, isbn);
+    isbnNumbers.add(isbn);
+    return book;
+  }
+};
+
+
+addBook("Harry","J:K", "123", true , 100)
+addBook("Harry","J:K", "123", true , 100)
+addBook("HaHirry","J:K", "435", true , 100)
+console.table(bookList)
+console.log(isbnNumbers.values())
